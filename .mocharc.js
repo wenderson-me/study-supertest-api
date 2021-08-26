f2ec50108af8a3d5ac32f258fe461a1dfd1a8980
Wenderson <wendrson22@gmail.com>
2021-08-26 00:21:47 -0300

    remove file

--- .mocharc.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  spec: ["test/**/*.spec.js"],
-  require: 'test/utils/global.js',
-  reporter: 'mochawesome',
-  reporterOptions: 'json=false,reportDir=report,reportFilename=index'
-}
